12142e909432b0e4fef55429c0a6b24f700112df
Yash Singh <yashbhatt499@gmail.com>
2026-03-04 11:35:04 +0100

    Add Chat History And New Chat Feature

--- streamlit_frontend_streaming.py
+++ streamlit_frontend_streaming.py
@@ -1,12 +1,43 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
 
 user_input =st.chat_input("Type Here")
-CONFIG = {'configurable':{'thread_id':'thread-1'}}
+
+def generate_id():
+    thread_id=uuid.uuid4()
+    return thread_id
+
+def reset_chat():
+    thread_id=generate_id()
+    st.session_state['thread_id']=thread_id
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history']=[]
+
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history']=[]
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id']=generate_id()
+
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads']=[]
+
+add_thread(st.session_state['thread_id'])
+
+st.sidebar.title('LangGraph Chatbot')
+
+if st.sidebar.button('New'):
+    reset_chat()
+
+st.sidebar.header('Conversations')
+for thread_id in st.session_state['chat_threads']:
+    st.sidebar.text(st.session_state['thread_id'])
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
@@ -18,12 +49,15 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
+    CONFIG = {'configurable':{'thread_id':st.session_state['thread_id']}}
+
+
     
     with st.chat_message('assistant'):
         ai_message = st.write_stream(
            message_chunk.content for message_chunk,metadata in chatbot.stream(
                 {'messages':[HumanMessage(content=user_input)]},
-                config={'configurable':{"thread_id":'thread-1'}},
+                config=CONFIG,
                 stream_mode='messages'
             )
         )
