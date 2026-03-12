dfeed1bd539165d48b7785c4c6873e2f6b7bcb01
Yash Singh <yashbhatt499@gmail.com>
2026-03-12 09:33:26 +0100

    Add Configuration for langsmith

--- langgraph_database_frontend.py
+++ langgraph_database_frontend.py
@@ -76,7 +76,12 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    CONFIG = {'configurable':{'thread_id':st.session_state['thread_id']}}
+    CONFIG = {'configurable':{'thread_id':st.session_state['thread_id']},
+              "metadata": {
+                  "thread_id":st.session_state["thread_id"],
+              },
+              "run_name":"chat_run"
+              }
 
 
     
